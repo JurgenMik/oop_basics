48b22a444b08550a1414a00ff12c3e5c1311cfb9
Jurgen Mikk <jurgen.mikk@khk.ee>
2021-11-15 09:49:27 +0200

    creating a constructor  + creating an object instance from constructor

---
+++ oop_basics.js
@@ -0,0 +1,27 @@
+
+function createNewPerson (name){
+    const obj = {};
+    obj.name = name;
+    obj.greeting = function(){
+        alert('Hi!' + obj.name);
+    };
+    return obj;
+}
+
+function Person(first,last,age,gender,interests) {
+    this.name = {
+        first: first,
+        last: last
+    };
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+    this.bio = function(){
+        alert(this.name.first + '' + this.name.last + 'is ' + this.age + 'years old. He likes' + this.interests[0] + 'and' + this.interests[1] + '.');
+    };
+    this.greeting = function(){
+        alert('Hi! Im ' + this.name.first + '.')
+    }
+}
+
+let person1 = new Person ('Bob', 'Smith', 32 , 'male', ['music', 'skiing']);
